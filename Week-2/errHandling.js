d6f76e69d69cd85a4628e3b384de681edc65f984
Syed Mujtaba <mujtabasyed486@gmail.com>
2025-06-19 17:38:25 +0500

    updated paradigm codes

--- Week-2/errHandling.js
+++ Week-2/errHandling.js
@@ -36,9 +36,28 @@ function asyncWithPromise() {
     });
 }
 
+function myFunction() {
+  const message = document.getElementById("p01");
+  message.innerHTML = "";
+  let x = document.getElementById("demo").value;
+  try {
+    if(x.trim() == "") throw "is empty";
+    if(isNaN(x)) throw "is not a number";
+    x = Number(x);
+    if(x > 10) throw "is too high";
+    if(x < 5) throw "is too low";
+  }
+  catch(err) {
+    message.innerHTML = "Error: " + err + ".";
+  }
+  finally {
+    document.getElementById("demo").value = "";
+  }
+}
 
 
 syncFunction();
 asyncFunction();
 asyncWithPromise();
 asyncFunctionwithCallback();
+myFunction();
